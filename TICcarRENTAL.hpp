3e91a659d04d7ed0394d4f53685eeb2f3a32ec2c
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-10-06 09:08:26 -0300

    Add files via upload

---
+++ TICcarRENTAL.hpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef usuario_H
+#define usuario_H
+#ifndef veiculo_H
+#define veiculo_H
+#ifndef aluguel_H
+#define aluguel_H
+#ifndef funcionario_H
+#define funcionario_H
+#ifndef cliente_H
+#define cliente_H
+
+using namespace std;
+
+class Usuario{
+    string CPF;
+    string nome;
+    string endereco;
+    string telefone;
+};
+
+class Cliente : public Usuario{
+    string CNH_cliente;
+
+};
+
+class Funcionario : public Usuario{
+    string CNH_funcionario;
+};
+
+class Veiculo {
+    string placa;
+    string marca;
+    string modelo;
+    int anodefabricacao;
+    float precoporday;
+};
+
+class Aluguel {
+    string id;
+    Veiculo veiculo;
+    Cliente cliente;
+    Funcionario funcionario;
+    string data_inicio;
+    string data_termino;
+    string data_devolucao;
+    float desconto;
+    float adicional;
+
+};
+
+#endif
+#endif
+#endif
+#endif
+#endif
